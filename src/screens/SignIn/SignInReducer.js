23f58d3e6b69146df4b67adb1281264599a6f351
Alfredo Sabino <alfredo_sabino@hotmail.com>
2020-07-02 16:45:35 -0300

    Sign in with api implemented

--- src/screens/SignIn/SignInReducer.js
+++ src/screens/SignIn/SignInReducer.js
@@ -1,14 +1,26 @@
 import { SIGN_IN } from './SignInActions';
+import { setAccount, setToken, setRefreshToken } from '../../helpers/account';
 
 const intialState = {
     account: null
 }
 
-export default function(state = intialState, action) {
+export default function (state = intialState, action) {
     const { type, payload } = action;
     switch (type) {
         case SIGN_IN:
-            return {...intialState, account: {...payload, success: true } };
+            const response = payload ? payload.data : null;
+            const account = response ? response.data : null;
+            const metadata = response ? response.metadata : null;
+
+            const token = metadata ? metadata.token : null;
+            const refreshToken = metadata ? metadata.refreshToken : null;
+
+            if (account) setAccount(account);
+            if (token) setToken(token);
+            if (refreshToken) setRefreshToken(refreshToken);
+
+            return { ...intialState, account };
         default:
             return state;
     }
--- src/screens/SignIn/index.js
+++ src/screens/SignIn/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { signIn } from './SignInActions';
 
 const SignIn = (props) => {
-    const { signIn } = props;
+    const { signIn, account } = props;
+
+    if (account) {
+        return <Redirect to="/manage/links" />
+    }
 
     const submitHandler = (e) => {
         e.preventDefault();
 
-        signIn({ email: 'alfredo_sabino@hotmail.com', password: '123456' })
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData);
+        signIn(data)
     };
 
     return (
@@ -18,16 +25,20 @@ const SignIn = (props) => {
                 <form onSubmit={submitHandler}>
                     <div className="form-group">
                         <label>Email</label>
-                        <input type="text" className="form-control" placeholder="Your Email" />
+                        <input type="text" className="form-control" placeholder="Your Email" name="email" />
                     </div>
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" className="form-control" placeholder="Your Password" />
+                        <input type="password" className="form-control" placeholder="Your Password" name="password" />
                     </div>
                     <div>
                         <button className="btn btn-primary btn-round">Submit</button>
                     </div>
                 </form>
+                <div className="container text-center fixed-bottom pb-5">
+                    <div className="text-muted">Don't have an Account?</div>
+                    <Link to="/sign-up">Sign Up</Link>
+                </div>
             </div>
         </div>
     );
